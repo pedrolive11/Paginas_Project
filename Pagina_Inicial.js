0bd8c5e024c1187dd50c61146c65dca95f81ea15
pedro <pedro.live11@outlook.com>
2023-09-11 11:53:55 -0300

    New

--- Pagina_Inicial.js
+++ Pagina_Inicial.js
@@ -8,3 +8,16 @@ window.addEventListener('scroll', function () {
 });
 
 
+// Obtém o elemento da caixa de pesquisa
+const searchBox = document.getElementById("searchBox");
+
+// Adiciona um ouvinte de evento quando o mouse entra na caixa de pesquisa
+searchBox.addEventListener("mouseenter", function () {
+    // Adiciona a classe "expanded" quando o mouse entra
+    searchBox.classList.add("expanded");
+
+    // Define um temporizador para remover a classe após 5 segundos
+    setTimeout(function () {
+        searchBox.classList.remove("expanded");
+    }, 5000); // 5000 milissegundos = 5 segundos
+});
